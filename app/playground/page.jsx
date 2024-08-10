96be13e4a69ba943abee43e204f4a53e3580f42d
chirag <chiragsinghal04@gmail.com>
2024-08-10 20:21:51 +0530

    -pl

--- app/playground/page.jsx
+++ app/playground/page.jsx
@@ -1,20 +1,11 @@
 "use client"
-import { useEffect } from 'react';
+import React from 'react'
 
-export default function Playground() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://www.jdoodle.com/assets/jdoodle-pym.min.js";
-    script.type = "text/javascript";
-    document.body.appendChild(script);
-  }, []);
-
+export default function page() {
   return (
-    <>
-  
-        <div data-pym-src="https://www.jdoodle.com/embed/v1/1e9cc8d7ad73a464"></div>
-        <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"> </script></>
-    
-    
-  );
+    <div>
+      playground
+      
+    </div>
+  )
 }
